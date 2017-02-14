519e896ed92eb4533cbbddb17b4b4d1cbe9116f0
Adrian Diaz <web.art@adriandiaz.ca>
2017-02-14 04:43:37 +0000

    implements dotevn

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const request = require('request');
 const getRepoContributors = require('./getRepoContributors');
-let GITHUB_TOKEN;
 let owner = process.argv[2];
 let repo = process.argv[3];
 
@@ -13,22 +12,19 @@ function downloadImageByURL(url, filePath) {
          .on('finish', (response) => console.log('Download complete! (' + arguments[1] + ')'));
 }
 
-function getApiToken(path) {
-  fs.readFile(path, function (err, data) {
-    GITHUB_TOKEN = data.toString();
-    if (owner && repo) {
-      getRepoContributors(owner, repo, GITHUB_TOKEN, (err, result) => {
-        for (entry of result) {
-          downloadImageByURL(entry.avatar_url, entry.login);
-        }
-      });
-    } else {
-      console.log('Usage: \n $ node download_avatars.js <owner> <repo>');
-    }
-  });
+function downloadAvatars() {
+  if (owner && repo) {
+    getRepoContributors(owner, repo, (err, result) => {
+      for (entry of result) {
+        downloadImageByURL(entry.avatar_url, entry.login);
+      }
+    });
+  } else {
+    console.log('Usage: \n $ node download_avatars.js <owner> <repo>');
+  }
 }
 
-getApiToken("./token.md");
+downloadAvatars();
 
 
 
--- getRepoContributors.js
+++ getRepoContributors.js
@@ -1,10 +1,10 @@
 const request = require('request');
-const GITHUB_USER = "adriandgr";
+const dotenv = require('dotenv').config();
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+const GITHUB_USER = process.env.GITHUB_USER;
 
-function getRepoContributors(repoOwner, repoName, gitToken, cb) {
-  // ...
-  let requestURL = `https://${GITHUB_USER}:${gitToken}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
-
+function getRepoContributors(repoOwner, repoName, cb) {
+  let requestURL = `https://${GITHUB_USER}:${GITHUB_TOKEN}@api.github.com/repos/${repoOwner}/${repoName}/contributors`;
   var options = {
     url: requestURL,
     headers: {
@@ -13,11 +13,9 @@ function getRepoContributors(repoOwner, repoName, gitToken, cb) {
   };
 
   request(options, function (error, response, body) {
-    // Do more stuff with 'body' here
     console.log('Response Status Code:', response.statusCode, '\n');
     var records = JSON.parse(body);
     cb(error, records);
-
   })
   .on('error', function (err) {
     throw err;
