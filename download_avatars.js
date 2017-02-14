34a99ae3dd4542a80d503469ce0444cc0e89e64e
Adrian Diaz <web.art@adriandiaz.ca>
2017-02-14 03:55:20 +0000

    downloaded avatar images

--- download_avatars.js
+++ download_avatars.js
@@ -1,7 +1,24 @@
 const fs = require('fs');
+const request = require('request');
 const getRepoContributors = require('./getRepoContributors');
 let GITHUB_TOKEN;
 
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+         console.log('Response Status Code:', response.statusCode);
+         console.log('Content-type:', response.headers['content-type'])
+         console.log('Downloading image...');
+       })
+       .pipe(fs.createWriteStream('./avatars/'+ filePath + '.jpg'))
+       .on('finish', function (response) {
+        console.log('Download complete!');
+       });
+}
 
 function getApiToken(path) {
   fs.readFile(path, function (err, data) {
@@ -9,7 +26,8 @@ function getApiToken(path) {
 
     getRepoContributors("jquery", "jquery", GITHUB_TOKEN, function(err, result) {
       for (entry of result) {
-        console.log(entry.avatar_url);
+        downloadImageByURL(entry.avatar_url, entry.login);
+
       }
     });
   });
