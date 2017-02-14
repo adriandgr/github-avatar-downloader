b8a49efaf90d77655326f24022f74633cbc7cb32
Adrian Diaz <web.art@adriandiaz.ca>
2017-02-14 05:46:46 +0000

    warns user if directory does not exist. creates directory in place

--- download_avatars.js
+++ download_avatars.js
@@ -3,18 +3,32 @@ const request = require('request');
 const getRepoContributors = require('./getRepoContributors');
 let owner = process.argv[2];
 let repo = process.argv[3];
+const filepath = './avatars/';
 
 
-function downloadImageByURL(url, filePath) {
+
+function downloadImageByURL(url, gitUser) {
   request.get(url)
          .on('error', (err) => { throw err; })
-         .pipe(fs.createWriteStream('./avatars/' + filePath + '.jpg'))
+         .pipe(fs.createWriteStream(filepath + gitUser + '.jpg'))
+         .on('error', (response) => {
+            if (response.code === 'ENOENT'){
+              console.log('directory', filepath, 'does not exist!');
+              // throw response;
+              //fs.mkdir(filepath)
+            }
+          })
          .on('finish', (response) => console.log('Download complete! (' + arguments[1] + ')'));
 }
 
 function downloadAvatars() {
+  if (!fs.existsSync(filepath)){
+    console.log('WARNING: directory', filepath, 'does not exist. Creating it interactively. \n')
+        fs.mkdir(filepath);
+      }
   if (owner && repo) {
     getRepoContributors(owner, repo, (err, result) => {
+
       for (entry of result) {
         downloadImageByURL(entry.avatar_url, entry.login);
       }
